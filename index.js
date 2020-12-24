5304ff4572d9a65013b894edcabdba6083c1f10b
Jetyas123 <75154155+Jetyas123@users.noreply.github.com>
2020-12-24 15:03:29 +0000

    e

---
+++ index.js
@@ -0,0 +1,137 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+const config = require('./config.json')
+const command = require('./command')
+
+client.on('ready', () => {
+  console.log('The client is ready!')
+
+  client.user.setActivity('Universal Hangout | !help', { type: 'WATCHING' });
+
+  command(client, 'ban', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('BAN_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.ban()
+        message.channel.send(`${tag} That user has been banned`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to ban.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+  //----------------------------------------------------------------------------------------------------kick
+
+  command(client, 'kick', (message) => {
+    const { member, mentions } = message
+
+    const tag = `<@${member.id}>`
+
+    if (
+      member.hasPermission('ADMINISTRATOR') ||
+      member.hasPermission('KICK_MEMBERS')
+    ) {
+      const target = mentions.users.first()
+      if (target) {
+        const targetMember = message.guild.members.cache.get(target.id)
+        targetMember.kick()
+        message.channel.send(`${tag} That user has kicked`)
+      } else {
+        message.channel.send(`${tag} Please specify someone to kick.`)
+      }
+    } else {
+      message.channel.send(
+        `${tag} You do not have permission to use this command.`
+      )
+    }
+  })
+})
+//----------------------------------------------------------------------------------------------------server info
+command(client, 'serverinfo', (message) => {
+    const { guild } = message
+
+    const { name, region, memberCount, owner, afkTimeout } = guild
+    const icon = guild.iconURL()
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`Server info for "${name}"`)
+      .setThumbnail(icon)
+      .addFields(
+        {
+          name: 'Region',
+          value: region,
+        },
+        {
+          name: 'Members',
+          value: memberCount,
+        },
+        {
+          name: 'Owner',
+          value: owner.user.tag,
+        }
+      )
+
+    message.channel.send(embed)
+})
+//----------------------------------------------------------------------------------------------------test
+command(client, ['test'], (message) => {
+    message.channel.send('😇')
+  })
+
+  command(client, 'servers', (message) => {
+    client.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `${guild.name} has a total of ${guild.memberCount} members`
+      )
+    })
+  })
+//----------------------------------------------------------------------------------------------------clear channel
+  command(client, ['cc', 'clearchannel'], (message) => {
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      message.channel.messages.fetch().then((results) => {
+        message.channel.bulkDelete(results)
+      })
+    }
+  })
+  //----------------------------------------------------------------------------------------------------help
+  command(client, 'help', (message) => {
+    const { guild } = message
+
+    const { name, region, memberCount, owner, afkTimeout } = guild
+    const icon = guild.iconURL()
+
+    const embed = new Discord.MessageEmbed()
+      .setTitle(`My commands:`)
+      .setThumbnail(icon)
+      .setColor('#800080')
+      .setDescription(`
+      These are my supported commands:
+      
+      **!help** - Displays the help menu
+      
+      **!kick** - kicks the mentioned member
+      
+      **!ban** - bans the mentioned member
+      
+      **!serverinfo** - displays the server info
+      
+      **!test** - tests if the bot is working
+      
+      **!cc , !clearchannel** - Bulk deletes messages
+      `)
+    message.channel.send(embed)
+})
+
+client.login(config.token)
